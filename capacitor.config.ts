22034683d7242954acfc2d41509bb57d6b12053a
dmitrykrasiev <dmitrykrasiev@gmail.com>
2021-07-18 18:50:17 +0400

    Added ionic and capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-tour-of-heroes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
